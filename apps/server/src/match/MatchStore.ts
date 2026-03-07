e4a530881a4dc2f3f22a80ef904b39f4f98af039
elijahzhao24 <elijahzhao24@gmail.com>
2026-03-07 15:43:38 -0800

    add attack cooldown

--- apps/server/src/match/MatchStore.ts
+++ apps/server/src/match/MatchStore.ts
@@ -45,6 +45,7 @@ export class MatchStore {
         ]),
       ),
       hitstunTicksByPlayerId: Object.fromEntries(match.playerIds.map((playerId) => [playerId, 0])),
+      attackCooldownTicksByPlayerId: Object.fromEntries(match.playerIds.map((playerId) => [playerId, 0])),
       latestSnapshot: null,
     });
     return match;
